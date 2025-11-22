f13b8309b48a39b931d7a71b2dd77d88ff360c3e
Anastasis Orfanidis <anastasios.orfanidis@gmail.com>
2025-11-22 13:57:04 +0000

    Update the App component to fetch the data from the games endpoint and to setState and to pass it as prop to the GamesList

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,35 @@
 import './App.css';
+import { GamesList, type ResponseShape } from './components/GamesList.tsx';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [games, setGames] = useState<ResponseShape[]>([
+    { id: '', winner: '', gameName: '', timeSpent: '' },
+  ]);
   const startGame = () => {
     console.log('start game');
   };
+  const getGames = async () => {
+    const response = await fetch('http://localhost:3000/api/v1/games');
+    if (response.ok) {
+      return await response.json();
+    }
+  };
+  useEffect(() => {
+    const fetchGames = async () => {
+      const games = await getGames();
+      if (games) {
+        setGames(games);
+      }
+    };
+    fetchGames();
+  }, []);
+
   return (
     <>
       <h1>Multiplayer Tic-Tac-Toe</h1>
       <button onClick={startGame}>New Game</button>
+      <GamesList gamesResult={games} />
     </>
   );
 }
